41255c4b72cf1d2ba064b71ea4e75381b4b17e53
naistrai <rizkynaistrai@gmail.com>
2024-05-12 22:49:39 +0000

    initial commit

--- setup.py
+++ setup.py
@@ -1,41 +1,46 @@
 from distutils.core import setup
-# read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / 'README.md').read_text()
 
 setup(
-    name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-    packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-    version = '0.1',      # Start with a small number and increase it with every change you make
-    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-    long_description=long_description,            # Give a long description about your library
+    name = 'quislingismz',
+    packages = ['quislingismz'],
+    version = '1.1',
+    license='MIT',
+    description = 'quislingismz',
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    author = 'YOUR NAME',                   # Type in your name
-    author_email = 'your.email@domain.com',      # Type in your E-Mail
-    url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+    author = 'Naistrai',
+    author_email = 'rizkynaistrai@gmail.com',
+    url = 'https://github.com/naistrai/quislingismz',
+    download_url = 'https://github.com/naistrai/quislingismz/archive/v_01.tar.gz',
     project_urls={
-        'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+        'Documentation': 'https://github.com/naistrai/quislingismz',
         'Funding': 'https://donate.pypi.org',
         'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/pypa/sampleproject/',
-        'Tracker': 'https://github.com/pypa/sampleproject/issues',
-    }, 
-    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ], 
+        'Source': 'https://github.com/naistrai/quislingismz/',
+        'Tracker': 'https://github.com/naistrai/quislingismz/issues',
+    },
+    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+            'validators',
+            'beautifulsoup4',
+            'discordautochat',
+            'discorudo',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+     dependencies= [
+        'discordautochat',
+        'discorudo'
+    ]
 )
